54a571236af4869f18c5757e573e90b6bd9d48f7
Tomás Real <tomasreal.art3d@gmail.com>
2025-03-21 13:39:48 +0000

    Add all projects

---
+++ LevelOne/MainIsHere.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class MainIsHere
+{
+        private static void Main(string[] args)
+    {
+        Console.WriteLine("Nice Day!");
+    }
+}
+namespace LevelOne
+{
+    public class MainIsHere
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Bad day?");
+        }
+    }
+}
---
+++ LevelTwo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using SixLabors.ImageSharp.Formats.Gif;
+using Spectre.Console;
+using SoGoodLib;
+
+namespace LevelTwo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CanvasImage image = new CanvasImage("tux.jpg");
+            image = image.MaxWidth(24);
+            string randomImage = "";
+            string n = "";
+            int Width = int.Parse(n);
+
+            if (args.Length >= 2)
+            {
+                Console.WriteLine(SoGoodClass.SoGoodMethod());
+                CanvasImage image2 = new CanvasImage(randomImage);
+                image2.MaxWidth(Width);
+                AnsiConsole.Write(image2);
+                return;
+            }
+            else if (args.Length == 0)
+            {
+                AnsiConsole.Write(image);
+                return;
+            }
+        }
+    }
+}
